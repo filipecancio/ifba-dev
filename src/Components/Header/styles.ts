693f74214f3deeb6cd59f07e0811483eea443627
cancio <filipe.cancio@gmail.com>
2022-05-26 18:04:02 -0300

    update heaser

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,16 +1,29 @@
 import styled from 'styled-components';
 import image from '../../util/night.svg';
+import { mobile } from "../../Styles/Breakpoints";
 
 export const Container = styled.div`
     width: 100%;
     height: 440px;
     display: flex;
-    justify-content: center;
+    justify-content: end;
+    align-items: center;
+    @media ${mobile.tablet} {
+        justify-content: center;
+    }
     background-image: url(${image});
+
 `;
 
-export const Content = styled.svg`
-    width:100%;
-    height: 440px;
-    fill:none;
+export const Content = styled.img`
+
+    max-width: 500px;
+    width: 60%;
+    object-fit: contain;
+    margin-right: 50px;
+    transition: 1s;
+    @media ${mobile.tablet} {
+        margin-top: 200px;
+        margin-right: 0px;
+    }
 `;
