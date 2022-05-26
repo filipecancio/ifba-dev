43198f319bce3b25ef783017ba4035c08628d524
cancio <filipe.cancio@gmail.com>
2022-05-26 17:48:44 -0300

    update header

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,13 +1,16 @@
 import styled from 'styled-components';
+import image from '../../util/night.svg';
 
 export const Container = styled.div`
     width: 100%;
+    height: 440px;
     display: flex;
     justify-content: center;
+    background-image: url(${image});
 `;
 
-export const Nuvem01 = styled.svg`
-    width:1080px;
+export const Content = styled.svg`
+    width:100%;
     height: 440px;
     fill:none;
 `;
