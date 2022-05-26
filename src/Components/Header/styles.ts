be3658b96347ede7b775d441dba237c5e3d4028a
cancio <filipe.cancio@gmail.com>
2022-05-26 17:36:01 -0300

    Add Header

--- src/Components/Header/styles.ts
+++ src/Components/Header/styles.ts
@@ -1,4 +1,13 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+`;
+
+export const Nuvem01 = styled.svg`
+    width:1080px;
+    height: 440px;
+    fill:none;
 `;
